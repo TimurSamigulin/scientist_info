232289f2761f452f425523b55e37d05423abe930
TimurSamigulin <47005114+TimurSamigulin@users.noreply.github.com>
2021-07-21 12:18:21 +0300

    habr user posts parser

--- habr_parser.py
+++ habr_parser.py
@@ -7,8 +7,7 @@ class HabrParser():
     """
     Класс для парсинга информации о пользователе с habr
     """
-    def get_profile_html(self, tag):
-        url = f'https://habr.com/ru/users/{tag}'
+    def get_profile_html(self, url):
 
         try:
             responce = requests.get(url)
@@ -19,10 +18,14 @@ class HabrParser():
         return soup
 
     def get_user_counters(self, soup):
-        carma = soup.find(href="https://habr.com/ru/info/help/karma/").div.text
-        rating = soup.find(href="https://habr.com/ru/info/help/karma/#rating").div.text
-        followers = soup.find(href="https://habr.com/ru/users/pawnhearts/subscription/followers/").div.text
-        follow = soup.find(href="https://habr.com/ru/users/pawnhearts/subscription/follow/").div.text
+        counters = {}
+
+        counters['carma'] = soup.find(href="https://habr.com/ru/info/help/karma/").div.text
+        counters['rating'] = soup.find(href="https://habr.com/ru/info/help/karma/#rating").div.text
+        counters['followers'] = soup.find(href="https://habr.com/ru/users/pawnhearts/subscription/followers/").div.text
+        counters['follow'] = soup.find(href="https://habr.com/ru/users/pawnhearts/subscription/follow/").div.text
+
+        return counters
 
     def get_user_profile_summary(self, soup):
         defination_list = soup.find('ul', 'defination-list')
@@ -34,6 +37,39 @@ class HabrParser():
 
         return summary
 
+    def get_user_posts(self, url):
+        url += '/posts/'
+        soup = self.get_profile_html(url)
+
+        posts = soup.find('div', 'posts_list').ul.findAll('li', 'content-list__item')
+
+        posts_href = []
+        for post in posts:
+            posts_href.append(post.article.h2.a['href'])
+
+        return posts_href
+
+    def get_url(self, tag):
+        return f'https://habr.com/ru/users/{tag}'
+
+    def get_user_info(self, tag):
+        url = self.get_url(tag)
+        soup = self.get_profile_html(url)
+
+        info = {}
+        counters = self.get_user_counters(soup)
+        for k, v in counters.items():
+            info[k] = v
+
+        summary = self.get_user_profile_summary(soup)
+        for k, v in summary.items():
+            info[k] = v
+
+        info['posts'] = self.get_user_posts(url)
+
+        return info
+
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
@@ -41,6 +77,4 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
 
     habr_parser = HabrParser()
-    soup = habr_parser.get_profile_html('pawnhearts')
-
-    # print(soup.findAll(href=re.compile("https://habr.com/ru/info/help/karma/")))
+    print(habr_parser.get_user_info('pawnhearts'))
